9b771a86e168d0628ee31c788ca77680dd1a2eb5
gsousa12 <wgsousa12@gmail.com>
2024-03-19 09:25:22 -0300

    poo add

---
+++ POO/POO/POO/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Instituicao instituicao = new Instituicao();
+
+            System.Console.WriteLine("Informe o nome da instituição");
+            instituicao.Nome = System.Console.ReadLine();
+
+            System.Console.WriteLine("Informe o endereço da instituição");
+            instituicao.Endereco = System.Console.ReadLine();
+
+            System.Console.WriteLine("===================================");
+
+            System.Console.WriteLine($"Obrigado por informar os dados para a {instituicao.Nome}");
+            System.Console.WriteLine("Pressione qualquer tecla para encerrar.");
+            System.Console.ReadKey();
+
+        }
+    }
+}
