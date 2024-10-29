67989038dc1e7ac9118e8b59e54ce38c5842f526
gsousa12 <wgsousa12@gmail.com>
2024-10-28 21:27:47 -0300

    del

--- Fundamentals/POO/POO/POO/Instituicao.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace POO
-{
-    class Instituicao                                       
-    {
-       public string Nome { get; set; }
-        public string Endereco { get; set;}
-    }
-}
